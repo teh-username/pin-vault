86d0c6bef4b5826591eb054e853f054018f6def7
LA Roberto <me@laroberto.com>
2018-03-27 11:15:54 +0800

    Move add button from header to bottom right of the screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { View, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 import EntryList from '../components/EntryList';
@@ -8,13 +9,6 @@ import { ADD_ENTRY, getEntries } from '../redux/modules/listings';
 class Home extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Entries',
-    headerRight: (
-      <Icon
-        name="add"
-        onPress={() => navigation.navigate('Entry', { action: ADD_ENTRY })}
-        iconStyle={{ right: 10 }}
-      />
-    ),
   });
 
   constructor(props) {
@@ -26,11 +20,44 @@ class Home extends React.Component {
     this.props.navigation.navigate('Details', { id });
   }
 
+  _renderAddButton(navigation) {
+    return (
+      <View key="home_add_button" style={styles.icon}>
+        <Icon
+          raised
+          reverse
+          name="plus"
+          type="font-awesome"
+          onPress={() => navigation.navigate('Entry', { action: ADD_ENTRY })}
+        />
+      </View>
+    );
+  }
+
+  _renderEntryList(entries) {
+    return (
+      <EntryList
+        key="home_entrylist"
+        entries={entries}
+        onPress={this.onEntryPress}
+      />
+    );
+  }
+
   render() {
-    const { entries } = this.props;
-    return <EntryList entries={entries} onPress={this.onEntryPress} />;
+    const { entries, navigation } = this.props;
+    return [this._renderEntryList(entries), this._renderAddButton(navigation)];
   }
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    justifyContent: 'flex-end',
+    alignSelf: 'flex-end',
+    right: 25,
+    bottom: 25,
+  },
+});
 
 const mapStateToProps = state => ({
   entries: getEntries(state),
