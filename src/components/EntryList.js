9bf035945b3ad940db1b11fe1aeaa6403652165e
LA Roberto <me@laroberto.com>
2018-03-27 10:54:53 +0800

    Add CTA when user has no entries yet

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, StyleSheet, Text } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 const EntryList = ({ entries, onPress }) => (
   <ScrollView>
-    <List>
-      {entries.map(([id, { name }]) => (
-        <ListItem
-          key={id}
-          title={name}
-          onPress={() => {
-            onPress(id);
-          }}
-        />
-      ))}
-    </List>
+    {entries.length === 0 ? (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Hmm, Looks like you have no entries yet.
+        </Text>
+        <Text style={styles.text}>
+          Hit the add button below to get started!
+        </Text>
+      </View>
+    ) : (
+      <List>
+        {entries.map(([id, { name }]) => (
+          <ListItem
+            key={id}
+            title={name}
+            onPress={() => {
+              onPress(id);
+            }}
+          />
+        ))}
+      </List>
+    )}
   </ScrollView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    top: 10,
+    left: 10,
+  },
+  text: {
+    fontSize: 15,
+  },
+});
 
 export default EntryList;
