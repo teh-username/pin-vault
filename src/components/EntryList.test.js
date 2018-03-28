ae50a5546aac4211a24483f699142f21b1afbc37
LA Roberto <me@laroberto.com>
2018-03-28 09:45:56 +0800

    Add tests for EntryList component

--- src/components/EntryList.test.js
+++ src/components/EntryList.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import EntryList from './EntryList';
+import { Text } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 describe('EntryList component', () => {
-  it('should render as expected', () => {
+  it('should render CTA if there are no entries yet', () => {
+    const wrapper = shallow(<EntryList entries={[]} onPress={() => {}} />);
+    expect(wrapper.find(Text).length).toEqual(2);
+    expect(wrapper.find(ListItem).length).toEqual(0);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render entries if there are any', () => {
     const sampleEntries = [
       ['md391', { name: 'Bank 1' }],
       ['krni8', { name: 'Bank 2' }],
@@ -11,6 +20,7 @@ describe('EntryList component', () => {
     const wrapper = shallow(
       <EntryList entries={sampleEntries} onPress={() => {}} />
     );
+    expect(wrapper.find(ListItem).length).toEqual(3);
     expect(wrapper).toMatchSnapshot();
   });
 });
