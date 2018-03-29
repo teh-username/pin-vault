3ef60953befd0657a3a961c603478f8cb12f8650
LA Roberto <me@laroberto.com>
2018-03-29 13:36:42 +0800

    Update settings component test

--- src/components/Settings.test.js
+++ src/components/Settings.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ListItem } from 'react-native-elements';
 
 import Settings from './Settings';
 
@@ -6,5 +7,20 @@ describe('Settings component test', () => {
   it('should render as expected', () => {
     const wrapper = shallow(<Settings />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should execute callback with the correct arguments', () => {
+    const callbackMock = jest.fn();
+    const wrapper = shallow(
+      <Settings
+        handleTogglePasscodeRequirement={callbackMock}
+        currentPasscode="wololo"
+        requirePasscode={false}
+      />
+    );
+    const firstItem = wrapper.find(ListItem).at(0);
+    firstItem.simulate('switch');
+    expect(callbackMock.mock.calls.length).toBe(1);
+    expect(callbackMock.mock.calls[0]).toEqual(['wololo', false]);
   });
 });
