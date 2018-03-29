4d423a12f600d38fc202e2e81d8084ce57438aeb
LA Roberto <me@laroberto.com>
2018-03-29 21:18:47 +0800

    Fix CTA text on android

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { ScrollView, View, StyleSheet, Text } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
-const EntryList = ({ entries, onPress }) => (
-  <ScrollView>
-    {entries.length === 0 ? (
-      <View style={styles.container}>
-        <Text style={styles.text}>
-          Hmm, Looks like you have no entries yet.
-        </Text>
-        <Text style={styles.text}>
-          Hit the add button below to get started!
-        </Text>
-      </View>
-    ) : (
+const EntryList = ({ entries, onPress }) =>
+  entries.length === 0 ? (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hmm, Looks like you have no entries yet.</Text>
+      <Text style={styles.text}>Hit the add button below to get started!</Text>
+    </View>
+  ) : (
+    <ScrollView>
       <List>
         {entries.map(([id, { name }]) => (
           <ListItem
@@ -25,12 +21,12 @@ const EntryList = ({ entries, onPress }) => (
           />
         ))}
       </List>
-    )}
-  </ScrollView>
-);
+    </ScrollView>
+  );
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     top: 10,
     left: 10,
   },
